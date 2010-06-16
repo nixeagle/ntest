025b5c4623ea80fe82863304ea23cc3336d73b56
James S <i@nixeagle.org>
2010-06-15 20:28:05 -0400

    Define TEST-CASE-PASSED-P and related helper functions.

--- ntest.lisp
+++ ntest.lisp
@@ -77,4 +77,31 @@ Tests are set at never-run at initialization, then when run they get set
         (run-test-case-expression (test-case-expected-function test-case)))
   test-case)
 
+(defun test-case-unexpected-failure-p (test-case)
+  "True if TEST-CASE contains an unexpected failure.
+
+A failure is considered unexpected when the `test-case-expected-result'
+does not contain a `condition' and the expression signals. What this means
+is a condition cannot be considered _unexpected_ if the expected-result of
+the test is itself a condition."
+  (declare (test-case test-case))
+  (and (typep (test-case-expression-result test-case) 'condition)
+       (not (typep (test-case-expected-result test-case) 'condition))))
+
+(defun run-test-case-result-predicate (test-case)
+  "Compares TEST-CASE's results with the test's defined predicate."
+  (declare (test-case test-case))
+  (funcall (test-case-result-predicate test-case)
+           (test-case-expression-result test-case)
+           (test-case-expected-result test-case)))
+
+(defun test-case-passed-p (test-case)
+  "True if the TEST-CASE passed."
+  (declare (test-case test-case))
+  (if (test-case-unexpected-failure-p test-case)
+      (values nil :unexpected-failure)
+      (let ((passedp (run-test-case-result-predicate test-case)))
+        (values passedp
+                (if passedp :pass :failure)))))
+
 ;;; END
